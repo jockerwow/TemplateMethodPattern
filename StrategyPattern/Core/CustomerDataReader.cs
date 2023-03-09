07cb7609d1df594735ea43f21abb9d5d3b2657f2
Mohamed Nagieb <jocker_wow_2@hotmail.com>
2023-03-09 09:57:00 +0200

    Add project files.

---
+++ StrategyPattern/Core/CustomerDataReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern.Core
+{
+    internal class CustomerDataReader
+    {
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return new[] {
+            new Customer(){
+                Id= 1,
+                Name="Mohamed Nagieb",
+                IsEligibleForDiscount=true
+            },
+            new Customer(){
+                Id= 2,
+                Name="Wael Said"
+            }
+            };
+        }
+    }
+}
---
+++ StrategyPattern/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StrategyPattern.Core;
+
+var dataReader = new CustomerDataReader();
+var customers = dataReader.GetCustomers();
+while (true)
+{
+    Console.WriteLine("Customer List: [1] Mohamed Nagieb [2] Wael Said");
+    Console.Write($"Enter Customer ID: ");
+    var customerId = int.Parse( Console.ReadLine() );
+    Console.Write("Enter Item Quantity: ");
+    var quantity = double.Parse( Console.ReadLine() );
+    Console.Write("Enter Unit Price: ");
+    var unitPrice = double.Parse( Console.ReadLine() );
+
+    var customer = customers.First(x => x.Id == customerId);
+    var invoice = new Invoice
+    {
+        Customer = customer,
+        Lines = new[]
+          {
+            new InvoiceLine { Quantity = quantity, UnitPrice = unitPrice }
+        },
+        DiscountPercentage = customer.IsEligibleForDiscount ? 0.02 : 0
+        /* Challenge: New Business Requirement:
+         * The discount needs to be based on Customer category:
+         * New "no discount applies", 
+         * Silver "5% discount for 10K+ invoices", 
+         * Gold "5% discount for 10K+ invoices", 
+         */
+    };
+    Console.WriteLine($"Invoice created for customer `{customer.Name}` with net price: {invoice.NetPrice}");
+    Console.WriteLine("Press any key to create another invoice");
+    Console.ReadKey();
+    Console.WriteLine("-------------------------------------");
+}
